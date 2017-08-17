6d71db93b8b0076040269c8625763df8044f0ffa
wmjide19911212@163.com <wmjide19911212@163.com>
2017-08-17 17:50:35 +0800

    增加component注解

---
+++ src/main/java/com/chapter3/CustomerDao.java
@@ -0,0 +1,15 @@
+package com.chapter3;
+
+import com.annotation.Component;
+
+/**
+ * Created by wm on 2017/8/17.
+ */
+@Component
+public class CustomerDao {
+
+    public  String test(){
+
+        return "test";
+    }
+}
