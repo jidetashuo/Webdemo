4601a64d46c95d5f816d42344ee66805340f5e70
wmjide19911212@163.com <wmjide19911212@163.com>
2017-08-18 10:17:18 +0800

    修改

--- src/main/java/com/chapter1/HelloServlet.java
+++ src/main/java/com/chapter1/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.chapter1;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,11 +14,17 @@ import java.util.Date;
 /**
  * Created by wm on 2017/8/3.
  */
-@WebServlet("/hello")
+@WebServlet(urlPatterns = "/hello",loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("HelloServlet init====");
+        super.init(config);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello get  ");
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
         req.setAttribute("currentTime", currentTime);
